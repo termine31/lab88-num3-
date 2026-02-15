8f06a8b4a44bf6645e90acd2fac1b9d3b12c0b43
termine1 <gsafdq.tn.qe12@gmail.com>
2026-02-15 15:06:05 +0700

    УПРАЖНЕНИЕ 3(2) Создаю 2 динамических массива разной длины, также создаю mass3 - единый массив Заполняю массивы  и вывожу изначально. Потом сортирую их, применяя к 1 массиву адресацию по индексам, а к 2 массиву адресацию по указателю Вывожу массив слиянием и очищаю память

--- lab88/lab88.cpp
+++ lab88/lab88.cpp
@@ -2,6 +2,7 @@
 //
 #include <cmath>
 #include <iostream>
+
 using namespace std; 
 int main()
 {
@@ -42,5 +43,59 @@ int main()
 		cout << "Num(" << i << ") = " << *(ptr1+i) << endl;
 	}
 	delete[] arr4;
+	cout << "-----------------------------------------------------------------\n";
+	// УПРАЖНЕНИЕ 3(2)
+	int size1 = 13;
+	int size2 = 7; 
+	int* mass1 = new int[size1];
+	int* mass2 = new int[size2];
+	// новый массив
+	int* mass3 = new int[size1 + size2];
+	
+	cout << "Массив №1: ";
+	for (int i = 0; i < size1; i++) {
+		mass1[i] = i*2;
+		cout << mass1[i] << " ";
+	}
+	cout << endl << endl;
+	cout << "Массив №2: ";
+	for (int i = 0; i < size2; i++) {
+		*(mass2+i) = i*3+23;
+		cout << mass2[i] << " ";
+	}
+	int i = 0; int  j = 0; int  k = 0;
+	cout << endl;
+	while (i < size1 && j < size2) {
+		if (mass1[i] < *(mass2+j)) {
+			mass3[k] = mass1[i];
+			i++;
+		}
+		else {
+			mass3[k] = *(mass2+ j);
+			j++;
+		}
+		k++;
+	}
+	
 
+// добавляем оставшиеся числа в массивах
+	while (i < size1) {
+		mass3[k] = mass1[i];
+		i++;
+		k++;
+	}
+	while (j < size2) {
+		mass3[k] = *(mass2+j);
+		j++;
+		k++;
+	}
+	// вывод
+	cout << endl << "Массивы после слияния: " << endl;
+	for (int i = 0; i < (size1 + size2);i++) {
+		cout << mass3[i] << " ";
+	}
+	cout << endl;
+	delete[] mass1;
+	delete[] mass2;
+	delete[] mass3;
 }
